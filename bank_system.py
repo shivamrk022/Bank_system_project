efa701e60e6ae6223beb2f4d53e45654d7c067b8
shivamrk022 <shivamaurya9702@gmail.com>
2026-02-23 04:02:07 +0530

    some changes

---
+++ bank_system.py
@@ -0,0 +1,26 @@
+class BankAccount:    #class name
+
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("Deposited:", amount)       
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print("Withdrawn:", amount)
+        else:
+            print("Insufficient balance")
+
+    def show_balance(self):
+        print("Account Holder:", self.name)
+        print("Balance:", self.balance)
+        
+acc1 = BankAccount("Shivam", 1000 )      #objects
+
+acc1.deposit(6000)               #function call 
+acc1.withdraw(39900)             #function call 
+acc1.show_balance()              #function call 
